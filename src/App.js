21897decd076c98233e72f936f9eb210b5598c05
CalvinChan <86409748+calvin1012@users.noreply.github.com>
2023-03-17 23:33:43 +0800

    start init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import "rsuite/dist/rsuite.min.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
